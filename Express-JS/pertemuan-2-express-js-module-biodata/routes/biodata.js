a58739839f2922e55db0da31a967efa2b6eb034b
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-05-11 21:22:47 +0700

    merubah file biodata.js agar sesuai dengan require yang di minta dari tugas pesilat

--- Express-JS/pertemuan-2-express-js-module-biodata/routes/biodata.js
+++ Express-JS/pertemuan-2-express-js-module-biodata/routes/biodata.js
@@ -2,21 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
- const { name, place, birthOfDate, address } = req.body;
+ const { name, 'tempat-lahir': place, 'tanggal-lahir': birthOfDate, alamat: address } = req.body;
  if (!name || !place || !birthOfDate || !address) {
   return res.status(400).send('Semua field harus diisi');
  }
 
- return res.status(200).send({ name, place, birthOfDate, address });
+ return res.status(200).send({ name, 'tempat-lahir': place, 'tanggal-lahir': birthOfDate, alamat: address });
 });
 
 router.get('/', (req, res) => {
- const { name, place, birthOfDate, address } = req.query;
+ const { name, 'tempat-lahir': place, 'tanggal-lahir': birthOfDate, alamat: address } = req.query;
+
  if (!name || !place || !birthOfDate || !address) {
   return res.status(400).send('Harap lengkapi parameter URL');
  }
 
- const biodata = { name, place, birthOfDate, address };
+ const biodata = {
+  name,
+  'tempat-lahir': place,
+  'tanggal-lahir': birthOfDate,
+  alamat: address,
+ };
 
  return res.status(200).send(biodata);
 });
